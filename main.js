1c65f17e47311d4c79def5f2d2c4ca25b4f0b7d5
Leonel Menci <minterger@gmail.com>
2023-03-02 16:38:11 -0300

    add 8-menu and 13-shopcart to index.html

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 const desktopMenu = document.querySelector(".desktop-menu");
-const email = document.querySelector(".email");
+const email = document.querySelector(".navbar-right .email");
+const menu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const cartButton = document.querySelector(".navbar-shopping-cart");
+const shopCart = document.querySelector("#shop-cart");
 
 email.addEventListener("click", () => {
-  desktopMenu.classList.toggle("inactive");
+  toggleClassElement(desktopMenu);
+  isElementOpenToggleClass(shopCart);
 });
+
+menu.addEventListener("click", () => {
+  toggleClassElement(mobileMenu);
+  isElementOpenToggleClass(shopCart);
+});
+
+cartButton.addEventListener("click", () => {
+  toggleClassElement(shopCart);
+  isElementOpenToggleClass(mobileMenu);
+  isElementOpenToggleClass(desktopMenu);
+});
+
+/**
+ * funcion para cambiar clase inactive a un elemento html del dom
+ * @param {object} e elemento html del dom
+ */
+const toggleClassElement = (e) => {
+  e.classList.toggle("inactive");
+};
+
+/**
+ * funcion para cerrar el elemento si este esta activo/abierto
+ * @param {object} e elemento html del dom
+ */
+const isElementOpenToggleClass = (e) => {
+  if (!e.classList.contains("inactive")) {
+    toggleClassElement(e);
+  }
+};
